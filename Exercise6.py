188b167e4c16364b2b178f08a99c8d53ae492d50
aditya1583 <aditya.uppu@gmail.com>
2020-10-11 09:57:05 -0400

    Ex6.py

---
+++ Exercise6.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+"""
+Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+"""
+
+
+#  a = ['A', 'd', 'i', 't', 'y', 'a' ] 
+#  b = ['a', 'y' ,'t', 't', 'd', 'A' ]
+
+# Initialize 2 Arrays
+
+a=[]
+b=[]
+
+# User input string
+
+string=str(input("Enter a word:"))
+
+
+# Convert to lower [ can be uppercase as well ]
+stringl=string.lower()
+
+
+# Read string and append to list a
+
+for i in stringl:
+    a.append(i)
+
+# Find length of a
+lengthA=len(a)
+
+
+# Insert it into list b in reverse order
+for x in a[-lengthA:]:
+    b.insert(0,x)
+
+print()
+
+# Evaluate lists a and b
+
+if a==b:
+    print("{}, is a palindrome".format(string))
+else:
+    print("{}, is not a palindrome".format(string))
+
+
+
+print()
