942f2a8a44871823af1a9404f60e9a92a2895ab5
aditya1583 <aditya.uppu@gmail.com>
2020-10-17 13:02:07 -0400

    Square of a Num list comprehension

---
+++ square_1line.py
@@ -0,0 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+# SQUARE of a number in a single line
+num = int(input("Enter a number to determine the square uptp that #:"))
+square = [ i**2 for i in range(num+1)]
+print(square)
