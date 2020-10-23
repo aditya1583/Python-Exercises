6f8b9b0c22583124812746fabf4ec9799e74c25a
aditya1583 <aditya.uppu@gmail.com>
2020-10-23 09:48:52 -0400

    Comment added

--- largeof3.py
+++ largeof3.py
@@ -1,3 +1,8 @@
+"""
+Implement a function that takes as input three variables, and returns the largest of the three. Do this without using the Python max() function!
+
+The goal of this exercise is to think about some internals that Python normally takes care of for us. All you need is some variables and if statements!
+"""
 #!/usr/bin/python3
 a = int(input())
 b = int(input())
